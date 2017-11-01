2d453e8829544f6b13ba9d3752958857ffd9e415
Pavel Studenik <studenik@varhoo.cz>
2017-11-01 01:19:09 +0100

    create default page

--- alphabet/settings.py
+++ alphabet/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.core',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'alphabet.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, "alphabet", "templates"),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "alphabet", "media")
--- alphabet/urls.py
+++ alphabet/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+import apps.core.views
+
+print settings.STATIC_ROOT, settings.STATIC_URL
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+    #url(r'^', apps.core.views.home, name="home"),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT, show_indexes=True)
